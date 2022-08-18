20387364bdbff3de6014032a12571cf450118f12
krishnamohan-yerrabilli <krishnamohan-yerrabilli@github.com>
2022-08-18 09:29:38 +0530

    Added a String

--- booking-app/main.go
+++ booking-app/main.go
@@ -5,10 +5,9 @@ import "fmt"
 func main() {
 	const Tickets_count = 50
 	var Tickets_available = 50
-	var conferenceTickets = "Go Conference"
+	var conferenceTickets = "Go_Conference"
 
-	fmt.Println("Welcome to our", conferenceTickets, "tickes we are so happy this site is for you")
+	fmt.Println("Welcome to our ", conferenceTickets, "tickes we are so happy this site is for you")
 	fmt.Println("Overall We had ", Tickets_count, "for the concert, and we have", Tickets_available, "tickets are still available")
 	fmt.Println("Get your tickets for the", conferenceTickets)
-
 }
