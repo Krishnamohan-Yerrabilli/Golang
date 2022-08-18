8dc6802f469cbc8eef01701ffe4996a7720b689b
krishnamohan-yerrabilli <krishnamohan-yerrabilli@github.com>
2022-08-18 12:38:59 +0530

    added array now

--- booking-app/main.go
+++ booking-app/main.go
@@ -4,23 +4,43 @@ import "fmt"
 
 func main() {
 	const Tickets_count = 50
-	Tickets_availabe := 45
-	conferenceTickets := "Go_conference"
+	Tickets_available := 50
+	conferenceName := "Go_conference"
+	var booking_list [50]string
 
-	fmt.Printf("\nTickets_count is a type of %T, and conferenceTickets is a type of %T\n", Tickets_count, conferenceTickets)
+	fmt.Printf("\nTickets_count is a type of %T, and conferenceName is a type of %T\n", Tickets_count, conferenceName)
 
-	fmt.Printf("\nWelcome to  our %v we are so happy this site is for you\n", conferenceTickets)
-	fmt.Printf("Overall We have %v tickets are opened for the concert, and we have %v tickets are still available\n", Tickets_count, Tickets_availabe)
-	fmt.Println("\nGet your tickets for the", conferenceTickets)
+	fmt.Printf("\nWelcome to  our %v we are so happy this site is for you\n", conferenceName)
+	fmt.Printf("Overall We have %v tickets are opened for the concert, and we have %v tickets are still available\n", Tickets_count, Tickets_available)
+	fmt.Println("\nGet your tickets for the", conferenceName)
 
-	var username string
+	var first_name string
 
-	var user_booked int
-	//username of a user
+	fmt.Println("Enter your first_name: ")
+	fmt.Scan(&first_name)
 
-	username = "mohan"
-	user_booked = 5
+	var last_name string
+	fmt.Println("Enter your last_name: ")
+	fmt.Scan(&last_name)
 
-	fmt.Printf("\n%v booked %v tickets for the concert. \n", username, user_booked)
+	var email_name string
+	fmt.Println("Enter your email: ")
+	fmt.Scan(&email_name)
+
+	var tickets_booked int
+	fmt.Println("Please enter how many tickets you want to book: ")
+	fmt.Scan(&tickets_booked)
+
+	fmt.Printf("Congratulations %v %v you booked %v tickets, you will get a mail at %v Thankyou", first_name, last_name, tickets_booked, email_name)
+
+	Tickets_available = Tickets_available - tickets_booked
+	booking_list[0] = first_name + " " + last_name
+
+	fmt.Printf("\nWhole array list: %v\n", booking_list)
+	fmt.Printf("The first value: %v\n", booking_list[0])
+	fmt.Printf("The Type : %T\n", booking_list)
+	fmt.Printf("The length of the array: %v\n", len(booking_list))
+
+	fmt.Printf("\nNow only %v tickets has been available for %v, have a great day!", Tickets_available, conferenceName)
 
 }
