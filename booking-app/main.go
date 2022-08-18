817b85c75e6a089e0657213fd9d758f677fdec74
krishnamohan-yerrabilli <krishnamohan-yerrabilli@github.com>
2022-08-18 09:40:37 +0530

    mohan bit change

--- booking-app/main.go
+++ booking-app/main.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	const Tickets_count = 50
 	var Tickets_available = 50
-	var conferenceTickets = "Go_Conference"
+	var conferenceTickets = " Go_Conference"
 
 	fmt.Println("Welcome to our", conferenceTickets, "tickes we are so happy this site is for you")
 	fmt.Println("Overall We had ", Tickets_count, "for the concert, and we have", Tickets_available, "tickets are still available")
