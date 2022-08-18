53fee8dd9e0201afa9f03cc963305d89a4795c5b
krishnamohan-yerrabilli <krishnamohan-yerrabilli@github.com>
2022-08-18 08:52:27 +0530

    booking app tickets

--- booking-app/main.go
+++ booking-app/main.go
@@ -1,5 +1,13 @@
 package main
 
+import "fmt"
+
 func main() {
-	print("Hello world!")
+
+	const Tickets_count = 100
+	var conferenceTickets = "Go Conference"
+	var 
+	fmt.Println("Welcome to our", conferenceTickets, "tickes we are so happy this site is for you")
+	fmt.Println("Get your tickets for the", conferenceTickets)
+
 }
