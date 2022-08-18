8dada08adabd8310bdc8ee37aeb521d53e23ef04
krishnamohan-yerrabilli <krishnamohan-yerrabilli@github.com>
2022-08-18 09:33:40 +0530

    small change

--- booking-app/main.go
+++ booking-app/main.go
@@ -7,7 +7,7 @@ func main() {
 	var Tickets_available = 50
 	var conferenceTickets = "Go_Conference"
 
-	fmt.Println("Welcome to our ", conferenceTickets, "tickes we are so happy this site is for you")
+	fmt.Println("Welcome to our", conferenceTickets, "tickes we are so happy this site is for you")
 	fmt.Println("Overall We had ", Tickets_count, "for the concert, and we have", Tickets_available, "tickets are still available")
 	fmt.Println("Get your tickets for the", conferenceTickets)
 }
